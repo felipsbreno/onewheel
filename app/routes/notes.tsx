c352e79a021120412eadf69a74ceb4c58ca7614e
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-19 18:18:31 -0300

    feat(notes): add new changes in screen of notes

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -20,15 +20,15 @@ export default function NotesPage() {
     <div className="flex h-full min-h-screen flex-col">
       <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
         <h1 className="text-3xl font-bold">
-          <Link to=".">Notes</Link>
+          <Link to=".">{user.name}</Link>
         </h1>
-        <p>{user.name}</p>
+
         <Form action="/logout" method="post">
           <button
             type="submit"
             className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
           >
-            Logout
+            Sair
           </button>
         </Form>
       </header>
